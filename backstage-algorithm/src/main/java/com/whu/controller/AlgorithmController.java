5968f3e796e57f83e6c4aaa7e8d547c71930c62a
hedon <hedon_glb@global.com>
2020-07-14 11:17:25 +0800

    添加算法管理后台 backstage-algorithm 模块，并且实现了openfeign跨服务调用

---
+++ backstage-algorithm/src/main/java/com/whu/controller/AlgorithmController.java
@@ -0,0 +1,37 @@
+package com.whu.controller;
+
+import com.results.CommonResult;
+import com.whu.service.AlgorithmFeignService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Hedon Wang
+ * @create 2020-07-14 10:50
+ */
+
+@RestController
+@RequestMapping("/backstage")
+public class AlgorithmController {
+
+
+    @Autowired
+    AlgorithmFeignService algorithmFeignService;
+
+    /**
+     * 分页查询算法
+     * @param pageNum
+     * @param pageSize
+     * @param keyWord
+     * @return
+     */
+    @GetMapping("/algorithms")
+    public CommonResult selectAllAlgorithms(@RequestParam(value = "pageNum",defaultValue = "1")Integer pageNum,
+                                            @RequestParam(value = "pageSize",defaultValue = "6")Integer pageSize,
+                                            @RequestParam(value = "keyWord")String keyWord){
+        return algorithmFeignService.selectAllAlgorithms(pageNum,pageSize,keyWord);
+    }
+}
---
+++ backstage-algorithm/src/main/java/com/whu/service/AlgorithmFeignService.java
@@ -0,0 +1,21 @@
+package com.whu.service;
+
+import com.results.CommonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Hedon Wang
+ * @create 2020-07-14 10:57
+ */
+@Service
+@FeignClient(value = "common-algorithm")
+public interface AlgorithmFeignService {
+
+    @GetMapping("/algorithms")
+    public CommonResult selectAllAlgorithms(@RequestParam(value = "pageNum",defaultValue = "1")Integer pageNum,
+                                            @RequestParam(value = "pageSize",defaultValue = "6")Integer pageSize,
+                                            @RequestParam(value = "keyWord")String keyWord);
+}
