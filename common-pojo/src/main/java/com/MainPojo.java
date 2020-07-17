5eac07feaeed2b18270c72c0b4835d062fdc8bea
origin <izumisakai@aliyun.com>
2020-07-17 10:38:58 +0800

    修改配置能打成jar包

---
+++ common-pojo/src/main/java/com/MainPojo.java
@@ -0,0 +1,11 @@
+package com;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+public class MainPojo {
+    public static void main(String[] args) {
+        //SpringApplication.run(MainPojo.class,args);
+    }
+}
---
+++ common-utils/src/main/java/UtilsMain.java
@@ -0,0 +1,5 @@
+public class UtilsMain {
+    public static void main(String[] args) {
+
+    }
+}
