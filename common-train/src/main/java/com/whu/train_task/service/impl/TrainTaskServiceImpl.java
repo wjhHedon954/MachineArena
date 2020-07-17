bf5063f7fcd2aefa9f8f094cbcb7061ef8589da6
origin <izumisakai@aliyun.com>
2020-07-17 16:05:06 +0800

    修改并测试新建训练作业接口，增加删除接口

--- common-train/src/main/java/com/whu/train_task/service/impl/TrainTaskServiceImpl.java
+++ common-train/src/main/java/com/whu/train_task/service/impl/TrainTaskServiceImpl.java
@@ -9,6 +9,8 @@ import com.whu.train_task.service.ITrainTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 /**
  * <p>
  * 服务实现类
@@ -28,19 +30,36 @@ public class TrainTaskServiceImpl extends ServiceImpl<TrainTaskMapper, TrainTask
      * 接口 6.2.1.1 创建训练作业
      * @author Yi Zheng
      * @create 2020-07-17 13:00
-     * @updator
-     * @update
-     * @param trainTask 从前端获取训练作业数据，根据数据创建训练作业
-     * @param trainTaskConf 从前端获取训练作业参数数据，根据数据创建训练作业参数
-     * @return  返回创建影响的行数，因为插入了两条数据使用所以使用一个整数数组
+     * @updator Yi Zheng
+     * @update 2020-7-17 16:00
+     * @param trainTask 训练作业
+     * @param trainTaskConf  训练作业参数
+     * @return  返回通用数据
      */
     @Override
-    public int[] addTrainTask(TrainTask trainTask, TrainTaskConf trainTaskConf) {
+    public int[] addTrainTask(TrainTask trainTask,TrainTaskConf trainTaskConf) {
         int i = trainTaskMapper.insert(trainTask);
 
         //训练参数的属性"train_task_id"是上面insert生成的id
         trainTaskConf.setTrainTaskId(trainTask.getTrainTaskId());
+
         int j = trainTaskConfMapper.insert(trainTaskConf);
+        System.out.println("j="+j);
         return new int[]{i,j};
+    }
+
+
+    /**
+     * 接口 6.2.1.2 根据ID删除训练作业
+     * @author Yi Zheng
+     * @create 2020-07-17 14:00
+     * @updator
+     * @update
+     * @param trainTaskID 删除的ID
+     * @return  返回删除的操作影响行数
+     */
+    @Override
+    public int deleteTrainTaskById(Integer trainTaskID) {
+        return trainTaskMapper.deleteById(trainTaskID);
     }
 }
