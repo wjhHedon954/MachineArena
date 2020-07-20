6bcc3cac2bca0a5fb2b652b81b3f86611e63d9a9
origin <izumisakai@aliyun.com>
2020-07-20 11:34:26 +0800

    模拟对接研发训练接口

--- common-model/src/main/java/com/whu/model/controller/Test.java
+++ common-model/src/main/java/com/whu/model/controller/Test.java
@@ -3,16 +3,20 @@ package com.whu.model.controller;
 import com.constants.ResultCode;
 import com.responsevo.TrainStartVO;
 import com.results.CommonResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Test {
+    //模拟研发端创建容器
     @PostMapping("/teststart")
     public CommonResult testStartTrain(@RequestBody TrainStartVO vo){
         if (vo==null)
             return CommonResult.fail(ResultCode.EMPTY_PARAM);
         return CommonResult.success().add("startContainer","be4ad49404e10b39441ecf4f2e9e6aed69d6e54666213db30819cd4c28c61d1f");
+    }
+    //模拟研发端删除容器
+    @DeleteMapping("/testdelete/{id}")
+    public CommonResult testDelete(@PathVariable("id") Integer id){
+        return CommonResult.success().add("id",id);
     }
 }
