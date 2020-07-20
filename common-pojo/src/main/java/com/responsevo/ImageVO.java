7565b16c0c97e1d98db9d5ed1eb531b4054f360d
thomas10011 <tanhuiri@126.com>
2020-07-20 10:52:40 +0800

    mysql连接添加时区设置；新增创建镜像接口

---
+++ common-pojo/src/main/java/com/responsevo/ImageVO.java
@@ -0,0 +1,37 @@
+package com.responsevo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.models.auth.In;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @ClassName ImageVO
+ * @Description 创建和修改镜像时使用的VO
+ * @Author thomas
+ * @Date 2020/7/20 1:39 上午
+ * @Version 1.0
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@ApiModel(value = "创建镜像时传递的参数")
+public class ImageVO {
+
+    Integer userId;
+
+    Integer aigorithmId;
+
+    Integer alogrithm_engine_id;
+
+    String algorithm_input_reflect;
+
+    String algorithm_output_reflect;
+
+    String algorithm_starter_URL;
+
+}
